4ae40856ceefb8048e53386b06d714787a5b7b45
Théo Jouvin <theo@supertolerator.com>
2022-04-27 16:09:59 -0700

    Adding numSort, numSortTest, required lib files

---
+++ numSort.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class numSort {
+
+    public static ArrayList<Integer> sort(File input) throws FileNotFoundException {
+        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<Integer> tempAL = new ArrayList<>();
+        Scanner sc = new Scanner(input);
+
+        while(sc.hasNext()) {
+                tempAL.add(sc.nextInt());
+        }
+
+        int[] tempA = new int[tempAL.size()];
+        
+        for(int j=0; j<tempA.length; j++) {
+            tempA[j] = tempAL.get(j);
+        }
+
+        for(int x = 0; x<tempA.length-1; x++) {
+            int min = x;
+    
+            for(int y = x+1; y<tempA.length; y++) {
+                if(tempA[y] < tempA[min]) min = y;
+            }
+
+            int num = tempA[min];
+            tempA[min] = tempA[x];
+            tempA[x] = num;
+
+        }
+
+        for(int z = 0; z<tempA.length; z++) nums.add(tempA[z]);
+        return nums;
+
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File input = new File(args[0]);
+        ArrayList<Integer> nums = sort(input);
+	    System.out.println(nums);
+    }
+}
---
+++ numSortTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class NSJU {
+
+    @Test
+    public void numSortTest() throws FileNotFoundException {
+        File input = new File("nums.txt");
+        assertEquals("[1, 2, 3, 4]", numSort.sort(input).toString());
+    }
+}
